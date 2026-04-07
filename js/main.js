21ecd087676da01669941f7b4875c4d7e74f523d
10no104 <smtdiski54@gmail.com>
2026-04-07 19:06:53 -0400

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// 버튼 클릭 시 해당 섹션으로 스크롤
-document.querySelectorAll('.navBtn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const targetId = btn.getAttribute('data-target');
-    const targetEl = document.getElementById(targetId);
-    targetEl.scrollIntoView({ behavior: 'smooth' });
-  });
-});
